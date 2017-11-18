6343fe762135f374760d7178b777ca435e9ee4d6
matrix8412 <susek.martin@gmail.com>
2017-11-18 23:53:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,16 @@
 
 mkdir /tmp/streamcloud/packages
 
-wget -O /tmp/streamcloud/packages/fdk-aac.deb https://streamcloud.homepi.org/packages/fdk-aac_201706131805-git-1_armhf.deb
+sudo wget -O /tmp/streamcloud/packages/fdk-aac.deb https://streamcloud.homepi.org/packages/fdk-aac_201706131805-git-1_armhf.deb
 
-apt-get update;
-apt-get upgrade;
-apt-get git htop iftop mc;
-dpkg -i /tmp/streamcloud/packages/fdk-aac.deb
+sudo apt-get update;
+sudo apt-get upgrade;
+sudo apt-get git htop iftop mc;
+sudo apt-get install libomxil-bellagio-dev 
+sudo dpkg -i /tmp/streamcloud/packages/fdk-aac.deb;
 
+sudo git clone https://github.com/FFmpeg/FFmpeg.git /tmp/streamcloud/ffmpeg;
 
+cd /tmp/streamcloud/ffmpeg
 
-git clone https://github.com/FFmpeg/FFmpeg.git /tmp/streamcloud/ffmpeg
+sudo ./configure --arch=armel --target-os=linux --enable-gpl --enable-omx --enable-omx-rpi --enable-libfdk-aac --enable-nonfree
