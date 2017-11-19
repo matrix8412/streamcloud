48ce8b3e27467fe5d96c731602fa3a27b0af1178
matrix8412 <susek.martin@gmail.com>
2017-11-19 15:47:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,14 @@ mkdir -p /opt/streamcloud/packages
 mkdir -p /var/www/html/config
 
 sudo cp /opt/streamcloud/cron_streamcloud /etc/cron.d/
+sudo cp /opt/streamcloud/web/* /var/www/html/
+chown -R www-data:www-data /var/www/html/*
+
 sudo service cron restart;
 
 sudo apt-get update;
 sudo apt-get -y upgrade;
-sudo apt-get install git htop iftop mc apache2 php libapache2-mod-php -y;
+sudo apt-get install git htop iftop mc apache2 php libapache2-mod-php php-xml -y;
 sudo apt-get install autoconf automake build-essential cmake git libass-dev libfreetype6-dev libsdl2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev mercurial pkg-config texinfo wget zlib1g-dev -y;
 sudo apt-get install yasm -y;
 sudo apt-get install libomxil-bellagio-dev libmp3lame-dev libvpx-dev libopus-dev libx264-dev libx265-dev libfdk-aac-dev -y;
