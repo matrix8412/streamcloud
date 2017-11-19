0998703a87f1afc01f57626fa0e189c3ea2da0c0
matrix8412 <susek.martin@gmail.com>
2017-11-19 23:58:09 +0100

    Update post_stream_stats.php

--- cron/scripts/post_stream_stats.php
+++ cron/scripts/post_stream_stats.php
@@ -7,7 +7,8 @@ $cloud_key = $xml_cloud->cloud_device_auth_key;
 $xml_stream = simplexml_load_file("/run/streamcloud_files/xml/ffmpeg.xml");
 $stream_id = $xml_stream->stream->stream_id;
 
-echo $stream_id;
+$stream_status = exec("ps aux |grep ffmpeg |grep -v grep | wc -l");
+if($stream_status > "0"){$stream_status = "on";}else{$stream_status = "off";}
 
 $aaa = exec("curl -s https://streamcloud.homepi.org/api/api.php --data \"action=stream_stats_rpi&sn=$sn&cloud_key=$cloud_key&stream_id=$stream_id&stream_status=$stream_status\"");
 ?>
