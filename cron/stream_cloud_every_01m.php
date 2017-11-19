7f3fd6d034f5c1d49f1497be033e5004a13d4558
matrix8412 <susek.martin@gmail.com>
2017-11-19 23:02:11 +0100

    Update stream_cloud_every_01m.php

--- cron/stream_cloud_every_01m.php
+++ cron/stream_cloud_every_01m.php
@@ -2,9 +2,9 @@
 exec("sudo mkdir -p /run/streamcloud_files");
 exec("sudo mkdir -p /run/streamcloud_files/xml");
 
-exec("php /opt/streamcloud/cron/scripts/get_streams.php");
-exec("php /opt/streamcloud/cron/scripts/stream_watchdog.php");
-exec("php /opt/streamcloud/cron/scripts/post_device_stats.php");
+exec("php /opt/streamcloud/cron/scripts/get_streams.php > /dev/null 2>&1 < /dev/null &");
+exec("php /opt/streamcloud/cron/scripts/stream_watchdog.php > /dev/null 2>&1 < /dev/null &");
+exec("php /opt/streamcloud/cron/scripts/post_device_stats.php > /dev/null 2>&1 < /dev/null &");
 
 ?>
 
