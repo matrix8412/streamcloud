29f5b0b9ac114840fabff70fe3439c54a5f39e33
matrix8412 <susek.martin@gmail.com>
2017-11-19 22:17:44 +0100

    Update stream_watchdog.php

--- cron/scripts/stream_watchdog.php
+++ cron/scripts/stream_watchdog.php
@@ -25,30 +25,48 @@ foreach($xml_string->stream as $stream){
     $audio_codec = $stream->audio_parameters->audio_codec;
     $audio_bitrate = $stream->audio_parameters->audio_bitrate;
     $audio_channels = $stream->audio_parameters->audio_channels;
-    $audio_sample_rate = $stream->audio_parameters->audio_sample_rate;
+    $audio_samplerate = $stream->audio_parameters->audio_sample_rate;
 
 
-    if($video_codec != "copy"){$ff_video_codec = " -c:v $video_codec";}else{$ff_video_codec = " -c:v copy";}
-    if($video_bitrate != "0"){$ff_video_bitrate = " -b:v ".$video_bitrate."k";}else{$ff_video_bitrate = "";}
-    if($video_min_bitrate != "0"){$ff_video_min_bitrate = " -minrate ".$video_min_bitrate."k";}else{$ff_video_min_bitrate = "";}
-    if($video_max_bitrate != "0"){$ff_video_max_bitrate = " -maxrate ".$video_max_bitrate."k";}else{$ff_video_max_bitrate = "";}
-    if($video_buffer_size != "0"){$ff_video_buffer_size = " -bufsize ".$video_buffer_size."k";}else{$ff_video_buffer_size = "";}
-    if($video_profile != "0"){$ff_video_profile = " -profile:v ".$video_profile;}else{$ff_video_profile = "";}
-    if($video_level != "0"){$ff_video_level = " -level ".$video_level;}else{$ff_video_level = "";}
+    if($video_codec != "copy"){$ff_video_codec = " -vcodec \"$video_codec\"";}else{$ff_video_codec = " -vcodec \"copy\"";}
+    if($video_bitrate != "0"){$ff_video_bitrate = " -b:v \"".$video_bitrate."k\"";}else{$ff_video_bitrate = "";}
+    if($video_min_bitrate != "0"){$ff_video_min_bitrate = " -minrate \"".$video_min_bitrate."k\"";}else{$ff_video_min_bitrate = "";}
+    if($video_max_bitrate != "0"){$ff_video_max_bitrate = " -maxrate \"".$video_max_bitrate."k\"";}else{$ff_video_max_bitrate = "";}
+    if($video_buffer_size != "0"){$ff_video_buffer_size = " -bufsize \"".$video_buffer_size."k\"";}else{$ff_video_buffer_size = "";}
+    if($video_profile != "0"){$ff_video_profile = " -profile:v \"".$video_profile."\"";}else{$ff_video_profile = "";}
+    if($video_level != "0"){$ff_video_level = " -level \"".$video_level."\"";}else{$ff_video_level = "";}
 
-    $ff_video_parameters = $ff_video_codec.$ff_video_bitrate.$ff_video_min_bitrate.$ff_video_max_bitrate.$ff_video_buffer_size.$ff_video_profile.$ff_video_level"";
+    $ff_video_parameters = $ff_video_codec.$ff_video_bitrate.$ff_video_min_bitrate.$ff_video_max_bitrate.$ff_video_buffer_size.$ff_video_profile.$ff_video_level;
 
-    if($audio_codec != "copy"){$ff_audio_codec = " -c:a $audio_codec";}else{$ff_sudio_codec = " -c:a copy";}
-    if($audio_bitrate != "0"){$ff_audio_bitrate = " -b:a ".$audio_bitrate."k";}else{$ff_audio_bitrate = "";}
-    if($audio_channels != "0"){$ff_audio_channels = " -b:v ".$audio_channels."k";}else{$ff_audio_channels = "";}
-    if($audio_samplerate != "0"){$ff_audio_samplerate = " -b:v ".$audio_samplerate."k";}else{$ff_audio_samplerate = "";}
+
+
+    if($audio_codec != "copy"){$ff_audio_codec = " -acodec \"".$audio_codec."\"";}else{$ff_sudio_codec = " -acodec \"copy\"";}
+    if($audio_bitrate != "0"){$ff_audio_bitrate = " -b:a \"".$audio_bitrate."k\"";}else{$ff_audio_bitrate = "";}
+    if($audio_channels != "0"){$ff_audio_channels = " -ac \"".$audio_channels."\"";}else{$ff_audio_channels = "";}
+    if($audio_samplerate != "0"){$ff_audio_samplerate = " -ar \"".$audio_samplerate."\"";}else{$ff_audio_samplerate = "";}
 
     $ff_audio_parameters = $ff_audio_codec.$ff_audio_bitrate;
 
-    $ffmpeg_cmd = "ffmpeg ".$input_url.$ff_video_parameters.$ff_audio_parameters." ".$output_url;
+
+
+
+    if($channel_name != ""){$ff_channel_name = " -metadata service_name=\"".$channel_name."\"";}else{$ff_channel_name = "";}
+
+    $ff_channel_parameters = $ff_channel_name;
+
+
+
+
+    $ffmpeg_cmd = "ffmpeg -i \"".$input_url."\"".$ff_video_parameters.$ff_audio_parameters.$ff_channel_parameters." ".$output_url;
 }
 
-
+$stream_proces_status = exec("ps aux |grep ffmpeg |grep -v grep | wc -l");
+if($stream_proces_status == "0"){
+    echo "Starting stream... \n";
+    exec("$ffmpeg_cmd > /dev/null 2>&1 < /dev/null &");
+}else{
+    echo "Stream running... \n";
+}
 
 
 
