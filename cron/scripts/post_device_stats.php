cec05af1a28b137435a0cc422663a742de425c06
matrix8412 <susek.martin@gmail.com>
2017-11-19 23:57:42 +0100

    Update post_device_stats.php

--- cron/scripts/post_device_stats.php
+++ cron/scripts/post_device_stats.php
@@ -3,6 +3,7 @@
 $hardware = exec("cat /proc/cpuinfo | grep Hardware | awk '{print $3}'");
 $revision = exec("cat /proc/cpuinfo | grep Revision | awk '{print $3}'");
 $sn = exec("cat /proc/cpuinfo | grep Serial | awk '{print $3}'");
+$localip = exec("hostname -I");
 $cpuTemp0 = exec("cat /sys/class/thermal/thermal_zone0/temp");
 $cpuTemp1 = $cpuTemp0/1000;
 $cpuTemp2 = $cpuTemp0/100;
@@ -27,6 +28,6 @@ foreach($dirs as $dir){
     $link_speed = exec("cat /sys/class/net/$dir/speed");
     exec("curl -s https://streamcloud.homepi.org/api/api.php --data \"action=device_network_activity&sn=$sn&net_interface=$dir&tx_bytes=$tx_bytes&rx_bytes=$rx_bytes&link_speed=$link_speed\"");
 }
-$aaa = exec("curl -s https://streamcloud.homepi.org/api/api.php --data \"action=device_keepalive_rpi&hardware=$hardware&revision=$revision&sn=$sn&cpuTemp=$cpuTemp&gpuTemp=$gpuTemp&loadavg1=$load_average1&loadavg5=$load_average5&loadavg15=$load_average15&sysuptime=$uptime&net_ifaces=$net_ifaces\"");
+$aaa = exec("curl -s https://streamcloud.homepi.org/api/api.php --data \"action=device_keepalive_rpi&hardware=$hardware&revision=$revision&sn=$sn&localip=localip&cpuTemp=$cpuTemp&gpuTemp=$gpuTemp&loadavg1=$load_average1&loadavg5=$load_average5&loadavg15=$load_average15&sysuptime=$uptime&net_ifaces=$net_ifaces\"");
 
 ?>
